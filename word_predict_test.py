d24d99410f620c992d0a92a60d1c6dc99d20b368
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-01 18:57:44 +0530

    added more test cases for learn method

--- word_predict_test.py
+++ word_predict_test.py
@@ -17,6 +17,32 @@ class TestWordPredict(unittest.TestCase):
         self.assertEqual(len(memory['this']), 1)
         self.assertEqual(len(memory['a']), 1)
         self.assertEqual(len(memory['cat']), 1)
+        self.assertEqual(memory['this']['is'], 1)
+        self.assertEqual(memory['a']['cat'], 1)
+        self.assertEqual(memory['is']['a'], 1)
+        self.assertEqual(memory['cat'][None], 1)
+        sentence = ['This', 'was', 'on', 'the', 'table']
+        test_object.learn(sentence)
+        memory = test_object.memory
+        self.assertEqual(len(memory), 8)
+        self.assertEqual(len(memory['a']), 1)
+        self.assertEqual(len(memory['on']), 1)
+        self.assertEqual(len(memory['this']), 2)
+        self.assertEqual(len(memory['is']), 1)
+        self.assertEqual(len(memory['cat']), 1)
+        self.assertEqual(len(memory['table']), 1)
+        self.assertEqual(len(memory['the']), 1)
+        self.assertEqual(len(memory['was']), 1)
+        self.assertEqual(memory['a']['cat'], 1)
+        self.assertEqual(memory['on']['the'], 1)
+        self.assertEqual(memory['this']['is'], 1)
+        self.assertEqual(memory['this']['was'], 1)
+        self.assertEqual(memory['cat'][None], 1)
+        self.assertEqual(memory['table'][None], 1)
+        self.assertEqual(memory['the']['table'], 1)
+        self.assertEqual(memory['was']['on'], 1)
+
+
 
 if __name__ == '__main__':
     unittest.main()
