fab7c466937d19d898db7cc88ed22c8758439a9b
omkarvijay5 <omkarvijay5@gmail.com>
2014-08-01 16:38:34 +0530

    added dummy learn and predict methods

--- word_predict.py
+++ word_predict.py
@@ -1,8 +1,18 @@
-import sys
+class WordPredict():
+
+    def learn(self, sentence):
+        pass
+
+    def predict(self, word):
+        pass
 
 if __name__ == '__main__':
-    try: 
-        method = sys.argv[1]
-    except:
-        print "Please add learn or predict trailing with a sentence or a word"
+    word_predict = WordPredict()
+    input_file = open('input.txt')
+    for line in input_file.readlines():
+        method = line.split()[0].lower()
+        sentence = ' '.join(line.strip().split()[1:])
+        if method == 'learn':
+            learn = getattr(word_predict, method)
+            learn(sentence)
 
